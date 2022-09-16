c17ded134e55f155ccdad24acd082fa24706e9ea
diegoandcontroll <lukasxdp@gmail.com>
2022-09-16 20:19:46 -0300

    feat: add sidebar component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,12 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
+import { Sidebar } from '../components';
 import { theme } from '../styles/theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
+      <Sidebar />
       <Component {...pageProps} />
     </ChakraProvider>
   );
