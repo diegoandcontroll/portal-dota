08a865f8699ac21e1f686196c22d840dcf76d805
diegoandcontroll <lukasxdp@gmail.com>
2022-09-16 19:47:48 -0300

    feat: add page heroes

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -3,7 +3,7 @@ import { extendTheme } from '@chakra-ui/react';
 export const theme = extendTheme({
   colors: {
     gray: {
-      '900': '#181B23',
+      '900': '#000000',
       '800': '#1F2029',
       '700': '#353646',
       '600': '#4B4D63',
