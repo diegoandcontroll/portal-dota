fd7337ad4acfe42b1e8695e8b3582fec9ea06029
diegoandcontroll <lukasxdp@gmail.com>
2022-09-16 22:47:18 -0300

    feat: add slider

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,22 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Flex, Heading } from '@chakra-ui/react';
 import type { NextPage } from 'next';
+import CaptionCarousel from '../components/Slider';
 
 const Home: NextPage = () => {
   return (
-    <Box as="main" p="4" color="white" ml="12">
-      Home
+    <Box as="main" p="4" color="white" pl="12">
+      <Heading
+        color="cyan.300"
+        fontWeight="light"
+        fontSize="6xl"
+        textAlign="center"
+      >
+        Welcome to Portal Dota
+      </Heading>
+      <Flex mt="20">
+        <CaptionCarousel />
+      </Flex>
+      <Flex mt="20"></Flex>
     </Box>
   );
 };
