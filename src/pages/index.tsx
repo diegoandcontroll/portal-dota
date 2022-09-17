5971db3f2f2b3c1813687ea2836e0e0f435b2d6e
diegoandcontroll <lukasxdp@gmail.com>
2022-09-16 23:45:46 -0300

    feat: create heroes and items section and embedvideo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,17 @@
+/* eslint-disable @next/next/no-img-element */
+/* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable react/no-unescaped-entities */
-import { Box, Flex, Heading, chakra } from '@chakra-ui/react';
+import {
+  Box,
+  Flex,
+  Heading,
+  chakra,
+  Wrap,
+  WrapItem,
+  VStack,
+} from '@chakra-ui/react';
 import type { NextPage } from 'next';
+
 import CaptionCarousel from '../components/Slider';
 
 const Home: NextPage = () => {
@@ -27,6 +38,96 @@ const Home: NextPage = () => {
           alvo. Pode ser usado para cancelar magias de concentração como
           Epicenter, Fiend's Grip e ETC.
         </chakra.span>
+      </Flex>
+      <Flex mt="8" justify="space-around" align="center">
+        <Box>
+          <Heading fontSize="1xl">Herois</Heading>
+          <VStack>
+            <Box py="4">
+              <Flex gap="6" justify="center" align="center">
+                <chakra.span fontSize="3xl">1°</chakra.span>
+                <Wrap cursor="pointer">
+                  <WrapItem>
+                    <img src="/hero.jpg" alt="imagehero" />
+                  </WrapItem>
+                </Wrap>
+              </Flex>
+            </Box>
+
+            <Box py="4">
+              <Flex gap="6" justify="center" align="center">
+                <chakra.span fontSize="3xl">2°</chakra.span>
+                <Wrap cursor="pointer">
+                  <WrapItem>
+                    <img src="/hero.jpg" alt="imagehero" />
+                  </WrapItem>
+                </Wrap>
+              </Flex>
+            </Box>
+
+            <Box py="4">
+              <Flex gap="6" justify="center" align="center">
+                <chakra.span fontSize="3xl">3°</chakra.span>
+                <Wrap cursor="pointer">
+                  <WrapItem>
+                    <img src="/hero.jpg" alt="imagehero" />
+                  </WrapItem>
+                </Wrap>
+              </Flex>
+            </Box>
+          </VStack>
+        </Box>
+        <Box>
+          <Heading fontSize="1xl">Items</Heading>
+          <VStack>
+            <Box py="4">
+              <Flex gap="6" justify="center" align="center">
+                <chakra.span fontSize="3xl">1°</chakra.span>
+                <Wrap cursor="pointer">
+                  <WrapItem>
+                    <img src="/hero.jpg" alt="imagehero" />
+                  </WrapItem>
+                </Wrap>
+              </Flex>
+            </Box>
+
+            <Box py="4">
+              <Flex gap="6" justify="center" align="center">
+                <chakra.span fontSize="3xl">2°</chakra.span>
+                <Wrap cursor="pointer">
+                  <WrapItem>
+                    <img src="/hero.jpg" alt="imagehero" />
+                  </WrapItem>
+                </Wrap>
+              </Flex>
+            </Box>
+
+            <Box py="4">
+              <Flex gap="6" justify="center" align="center">
+                <chakra.span fontSize="3xl">3°</chakra.span>
+                <Wrap cursor="pointer">
+                  <WrapItem>
+                    <img src="/hero.jpg" alt="imagehero" />
+                  </WrapItem>
+                </Wrap>
+              </Flex>
+            </Box>
+          </VStack>
+        </Box>
+      </Flex>
+      <Flex justify="center" mt="4">
+        Top 15
+      </Flex>
+      <Flex justify="center" align="center" mt="4">
+        <Box
+          as="iframe"
+          src="https://www.youtube.com/embed/Q3mly14qwfU?start=17"
+          width="70%"
+          sx={{
+            aspectRatio: '16/9',
+          }}
+          allowFullScreen
+        />
       </Flex>
     </Box>
   );
