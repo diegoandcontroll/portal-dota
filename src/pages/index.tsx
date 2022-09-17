8f35231a4385bdbafcbf15ec409b6a392e7c230b
diegoandcontroll <lukasxdp@gmail.com>
2022-09-16 22:48:09 -0300

    feat: git add curiosity

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,33 @@
-import { Box, Flex, Heading } from '@chakra-ui/react';
+/* eslint-disable react/no-unescaped-entities */
+import { Box, Flex, Heading, chakra } from '@chakra-ui/react';
 import type { NextPage } from 'next';
 import CaptionCarousel from '../components/Slider';
 
 const Home: NextPage = () => {
   return (
-    <Box as="main" p="4" color="white" pl="12">
+    <Box as="main" color="white" pl="12">
       <Heading
         color="cyan.300"
         fontWeight="light"
-        fontSize="6xl"
+        fontSize={['3xl', '6xl']}
         textAlign="center"
       >
         Welcome to Portal Dota
       </Heading>
-      <Flex mt="20">
+      <Flex mt="4">
         <CaptionCarousel />
       </Flex>
-      <Flex mt="20"></Flex>
+      <Flex mt="8" align="center">
+        <Heading color="gray.200" fontSize="2xl">
+          Curiosidades:
+        </Heading>
+
+        <chakra.span color="gray.200" fontSize="1xl" px="8">
+          Você sabia que a magia Chain Frost da Lich da ministun no primeiro
+          alvo. Pode ser usado para cancelar magias de concentração como
+          Epicenter, Fiend's Grip e ETC.
+        </chakra.span>
+      </Flex>
     </Box>
   );
 };
